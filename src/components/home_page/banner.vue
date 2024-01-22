7319d700c10e4128d5684442725269e540598165
ahmad banikamali <ahmad.banikamali@gmail.com>
2024-01-22 21:13:13 +0330

    INIT

--- src/components/home_page/banner.vue
+++ src/components/home_page/banner.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-  $END$
+    <div class="h-screen bg-banner bg-cover bg-center">
+
+    </div>
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Banner from "@/components/home_page/banner.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <banner></banner>
 </template>
